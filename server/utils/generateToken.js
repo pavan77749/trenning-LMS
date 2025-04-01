b8979c8ddc4ee7c85d630bec9701a2c0aaf129d4
pavan77749 <pavangpay05@gmail.com>
2025-04-01 23:08:49 +0530

    fix(generateToken): add secure and sameSite options

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,19 +1,20 @@
 import jwt from "jsonwebtoken";
 
-
 export const generateToken = (res, user, message) => {
   const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
     expiresIn: "1d",
   });
-  return res
-    .status(200)
-    .cookie("token", token, {
-      httpOnly: true,
-      sameSite: "strict",
-      maxAge: 24 * 60 * 60 * 1000, //1day
-    }).json({
-        success:true,
-        message,
-        user
-    })
+  
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // true in production
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // 'none' allows cross-site cookies
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  });
+  
+  return res.status(200).json({
+    success: true,
+    message,
+    user
+  });
 };
