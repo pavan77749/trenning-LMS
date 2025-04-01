452526fdeac854079d11f01e725df1a7be51ddfa
pavan77749 <pavangpay05@gmail.com>
2025-04-01 23:15:52 +0530

    refactor(api): update COURSE_PROGRESS_API to use environment variable

--- client/src/features/api/cousreProgressApi.js
+++ client/src/features/api/cousreProgressApi.js
@@ -1,7 +1,10 @@
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PROGRESS_API = "http://localhost:8080/api/v1/progress";
+const COURSE_PROGRESS_API = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/api/v1/progress` 
+  : "https://trenning-lms.onrender.com/api/v1/progress";
+
 
 export const courseProgressApi = createApi({
   reducerPath: "courseProgressApi",
